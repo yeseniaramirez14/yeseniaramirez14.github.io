283f7c915ed35a01f84a58d7f368aabc5ed755bb
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-07-30 04:14:30 -0500

    favicon and title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
-import { Footer } from './components/Footer';
-import { Contact } from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -15,7 +12,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      {/* <Contact /> */}
     </div>
   );
 }
