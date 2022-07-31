05a4f6d9dcd7004b3ebae634ac70f1fa3c1834e3
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-07-31 00:49:35 -0500

    tweaked the sizing for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -25,64 +25,64 @@ export const Skills = () => {
           </Col>
         </Row>
         <Row xs={3} sm={3} md={5} lg={5} xl={5} xxl={5}>
-          <Col xs={3} sm={3} >
-            <div className="item">
-              <SiJavascript size={"5em"} />
+          <Col className="item-bx" xs={3} sm={3} >
+            <div className="align-self-center item" >
+              <SiJavascript size={"5em"}/>
               <h5>JavaScript</h5>
             </div>
           </Col>
-          <Col xs={3} sm={3}>
-            <div className="item">
-              <SiPython size={"5em"} />
-              <h5>Python</h5>
+          <Col className="item-bx" xs={3} sm={3}>
+            <div className="align-self-center item">
+              <SiPython size={"5em"}/>
+            <h5>Python</h5>
             </div>
           </Col>
-          <Col xs={3} sm={3}>
-            <div className="item">
-              <SiHtml5 size={"5em"} />
-              <h5>HTML</h5>
+          <Col className="item-bx" xs={3} sm={3}>
+            <div className="align-self-center item">
+              <SiHtml5 size={"5em"}/>
+             <h5>HTML</h5>
             </div>
           </Col>
-          <Col xs={3} sm={3}>
-            <div className="item">
-              <SiCss3 size={"5em"} />
-              <h5>CSS</h5>
+          <Col className="item-bx" xs={3} sm={3}>
+            <div className="align-self-center item">
+              <SiCss3 size={"5em"}/>
+            <h5>CSS</h5>
             </div>
           </Col>
-          <Col>
+          <Col className="item-bx">
             <div className="item">
-              <SiBootstrap size={"5em"} />
+              <SiBootstrap size={"5em"}/>
               <h5>Bootstrap</h5>
             </div>
           </Col>
-          <Col>
+          <Col className="item-bx">
             <div className="item">
-              <SiReact size={"5em"} />
-              <h5>React</h5>
+              <SiReact size={"5em"}/>
+             <h5>React</h5>
             </div>
           </Col>
-          <Col>
+          <Col className="item-bx">
             <div className="item">
-              <SiDjango size={"5em"} />
-              <h5>Django</h5>
+              <SiDjango size={"5em"}/>
+            <h5>Django</h5>
             </div>
           </Col>
-          <Col>
+          <Col className="item-bx">
             <div className="item">
-              <SiPostgresql size={"5em"} />
+              <SiPostgresql size={"5em"}/>
               <h5>PostgreSQL</h5>
             </div>
           </Col>
-          <Col>
+          <Col className="item-bx">
             <div className="item">
-              <SiFastapi size={"5em"} />
-              <h5>FastAPI</h5>
+              <SiFastapi size={"5em"}/>
+             <h5>FastAPI</h5>
             </div>
           </Col>
-          <Col>
+          <Col className="item-bx">
             <div className="item">
-              <SiDocker size={"5em"} />
-              <h5>Docker</h5>
+              <SiDocker size={"5em"}/>
+            <h5>Docker</h5>
             </div>
           </Col>
         </Row>
